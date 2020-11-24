e883c1f10f13e0e11380cde254098a78e4b82fc7
Rathijit Paul <alienpapon009@gmail.com>
2020-11-24 22:02:40 +0600

    Add WebSocket Service

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const socketio = require('socket.io');
+const http = require('http');
 
 const initDB = require("./db/mongoose");
 
@@ -12,6 +14,13 @@ var corsOptions = {
 };
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
 
 initDB();
 
@@ -25,4 +34,77 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/messages", messageRouter);
 
-module.exports = app;
+const auth = async (token) => {
+    const jwt = require("jsonwebtoken");
+    const User = require("./models/user");
+    const JWT_SECRET = process.env.JWT_SECRET;
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = await User.findOne({
+        _id: decoded._id,
+        "tokens.token": token,
+    });
+
+    if(!user) {
+        return {
+            error: "Authentication unsuccessful.",
+        };
+    }
+
+    return {
+        user
+    };
+}
+
+io.on("connect", socket => {
+    const {sendMessage, getInbox, getAllMessages} = require("./controllers/message");
+
+    socket.on("joinMessages", async (data, cb) => {
+        const userData = await auth(data.token);
+        if(userData.error) {
+            cb(userData.error);
+        }
+
+        const messageData = await getAllMessages(userData.user._id);
+
+        socket.join(userData.user._id.toString());
+        io.to(userData.user._id.toString()).emit("getMessages", {messages: messageData.messages});
+    });
+    
+    socket.on("joinInbox", async (data, cb) => {
+        const userData = await auth(data.token);
+        if(userData.error) {
+            cb(userData.error);
+        }
+
+        const inboxData = await getInbox(userData.user._id, data.username);
+
+        if(inboxData.error) {
+            cb(inboxData.error);
+        }
+
+        socket.join(inboxData.message._id.toString());
+        io.to(inboxData.message._id.toString()).emit("getInbox", {inbox: inboxData.message});
+    });
+
+    socket.on("sendText", async (data, cb) => {
+        const userData = await auth(data.token);
+        if(userData.error) {
+            cb(userData.error);
+        }
+
+        const inboxData = await sendMessage(userData.user, data.username, data.messageText);
+        if(inboxData.error) {
+            cb(inboxData.error);
+        }
+
+        const messageData = await getAllMessages(userData.user._id);
+
+        io.to(inboxData.message._id.toString()).emit("getInbox", {inbox: inboxData.message});
+        io.to(userData.user._id.toString()).emit("getMessages", {messages: messageData.messages});
+        io.to(inboxData.receiver._id.toString()).emit("getMessages", {messages: messageData.messages});
+        
+    });
+});
+
+module.exports = server;
--- src/controllers/message.js
+++ src/controllers/message.js
@@ -129,9 +129,163 @@ const getMessage = async (req, res) => {
     }
 };
 
+const sendMessage = async (sender, receiverUsername, messageText) => {
+    
+    const receiver = await User.findOne({
+        username: receiverUsername,
+    });
+
+    if (!receiver) {
+        return {
+            error: "Invalid update message request!",
+        };
+    }
+
+    let message = await Message.findOne({
+        user1: receiver._id,
+        user2: sender._id,
+    });
+
+    if(!message) {
+        message = await Message.findOne({
+            user1: sender._id,
+            user2: receiver._id,
+        });
+    }
+
+    if(!message) {
+        message = new Message({
+            user1: sender._id,
+            user2: receiver._id,
+        });
+    }
+
+    message.message.push({
+        sender: sender._id,
+        messageText: messageText,
+    });
+
+    await message.save();
+    await message.populate([
+        {
+            path: "user1",
+            select: ["username", "fullname", "profileImage"],
+        },
+        {
+            path: "user2",
+            select: ["username", "fullname", "profileImage"],
+        },
+        {
+            path: "message.sender",
+            select: ["username", "fullname", "profileImage"],
+        }
+    ]).execPopulate();
+
+    return {
+        message,
+        receiver,
+    };
+};
+
+const getInbox = async (user1, username) => {
+    
+    const user2 = await User.findOne({
+        username: username,
+    });
+
+    if (!user2) {
+        return {
+            error: "Invalid get message request!",
+        };
+    }
+    
+    let message = await Message.findOne({
+        user1: user1,
+        user2: user2,
+    });
+    if(!message) {
+        message = await Message.findOne({
+            user1: user2,
+            user2: user1,
+        });
+    }
+
+    if(!message) {
+        return {
+            error: "No Message Found"
+        };
+    }
+    else{
+        await message.populate([
+            {
+                path: "user1",
+                select: ["username", "fullname", "profileImage"],
+            },
+            {
+                path: "user2",
+                select: ["username", "fullname", "profileImage"],
+            },
+            {
+                path: "message.sender",
+                select: ["username", "fullname", "profileImage"],
+            }
+        ]).execPopulate();
+    }
+
+    return {
+        message,
+    };
+};
+
+const getAllMessages = async (user) => {
+    
+    let messages1 = await Message.find({
+        user1: user,
+    }).sort({updatedAt: -1}).populate([
+        {
+            path: "user1",
+            select: ["username", "fullname", "profileImage"],
+        },
+        {
+            path: "user2",
+            select: ["username", "fullname", "profileImage"],
+        },
+        {
+            path: "message.sender",
+            select: ["username", "fullname", "profileImage"],
+        }
+    ]).exec();
+
+    let messages2 = await Message.find({
+            user2: user,
+        }).sort({updatedAt: -1}).populate([
+            {
+                path: "user1",
+                select: ["username", "fullname", "profileImage"],
+            },
+            {
+                path: "user2",
+                select: ["username", "fullname", "profileImage"],
+            },
+            {
+                path: "message.sender",
+                select: ["username", "fullname", "profileImage"],
+            }
+        ]).exec();
+
+    const messages = [...messages1, ...messages2];
+
+    return {
+        messages: messages,
+    };
+};
+
 const messageController = {
     updateMessage,
     getMessage,
+    sendMessage,
+    getInbox,
+    getAllMessages,
 };
 
 module.exports = messageController;

551923fff0b986338df48dad5db5578746467399
Rathijit Paul <alienpapon009@gmail.com>
2020-11-24 23:00:57 +0600

    Fix Email Service

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
     }
 });
--- src/services/email.js
+++ src/services/email.js
@@ -3,12 +3,12 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.EMAIL_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
-    // sgMail.send({
-    //     to: email,
-    //     from: process.env.EMAIL_ADDRESS,
-    //     subject: 'Thanks for joining in!',
-    //     text: `Welcome to the Travel Talk community, ${name}.`
-    // });
+    sgMail.send({
+        to: email,
+        from: process.env.EMAIL_ADDRESS,
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the Travel Talk community, ${name}.`
+    });
 };
 
 const sendCancelationEmail = (email, name) => {

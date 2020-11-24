484e389b441d01bbe736b6cb60911afce8f20ecf
Rathijit Paul <alienpapon009@gmail.com>
2020-11-24 23:54:08 +0600

    Add Cors Settings

--- src/app.js
+++ src/app.js
@@ -33,6 +33,24 @@ app.use(cors(corsOptions));
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/messages", messageRouter);
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://traveltalkcommunity.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const auth = async (token) => {
     const jwt = require("jsonwebtoken");

1d482614437f93ac8af0c418c03a45b094bbe44b
Rathijit Paul <alienpapon009@gmail.com>
2020-11-25 10:47:25 +0600

    Cors fixed

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ const userRouter = require("./routers/user");
 const postRouter = require("./routers/post");
 const messageRouter = require("./routers/message");
 
-// var corsOptions = {
-//     origin: "*"
-// };
+var corsOptions = {
+    origin: "https://traveltalkcommunity.herokuapp.com/",
+};
 
 const app = express();
 app.use(function (req, res, next) {
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb', extended: true}));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);

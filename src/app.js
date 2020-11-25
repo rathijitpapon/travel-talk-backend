889ca622c3af8b2a9054e9849afd2781e07fb0e6
Rathijit Paul <alienpapon009@gmail.com>
2020-11-25 10:01:52 +0600

    Add Cors Settings

--- src/app.js
+++ src/app.js
@@ -14,6 +14,26 @@ var corsOptions = {
 };
 
 const app = express();
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
@@ -34,24 +54,6 @@ app.use(cors(corsOptions));
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/messages", messageRouter);
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 const auth = async (token) => {
     const jwt = require("jsonwebtoken");
